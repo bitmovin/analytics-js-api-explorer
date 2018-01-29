091b1a6384e9a22b6eb838b115a95575c7d3fb05
Clemens Helm <clemens.helm@gmail.com>
2018-01-29 12:46:17 +0100

    Convert tabs to spaces.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,30 @@ import runJs from './runJs';
 import moment from 'moment';
 
 class App extends Component {
-	state = {
-		js: '',
-	};
+  state = {
+    js: '',
+  };
 
-	updateJs = ({ target }) => {
-		this.setState({ js: target.value });
-	};
+  updateJs = ({ target }) => {
+    this.setState({ js: target.value });
+  };
 
-	runJs = (event) => {
-		event.preventDefault();
+  runJs = (event) => {
+    event.preventDefault();
 
-		runJs(this.state.js, { moment });
-	}
+    runJs(this.state.js, { moment });
+  }
 
   render() {
     return (
       <div className="App">
-				<form onSubmit={this.runJs}>
-					<input
-						onChange={this.updateJs}
-						value={this.state.js}
-					/>
-					<button>Run</button>
-				</form>
+        <form onSubmit={this.runJs}>
+          <input
+            onChange={this.updateJs}
+            value={this.state.js}
+          />
+          <button>Run</button>
+        </form>
       </div>
     );
   }
--- src/runJs.js
+++ src/runJs.js
@@ -1,3 +1,3 @@
 export default (js, { queryBuilder, moment }) => {
-	eval(js);
+  eval(js);
 }
