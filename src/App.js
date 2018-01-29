098dfa03188853089cb4f3723bd906f6676fd76a
Clemens Helm <clemens.helm@gmail.com>
2018-01-29 14:57:14 +0100

    Add bootstrap styling.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/latest/css/bootstrap.min.css" />
         <Authenticated>
           <Main>
           </Main>
