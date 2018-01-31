e2f4f452a72d6d6fbfc6c9e257b1371538c2ea6e
Clemens Helm <clemens.helm@gmail.com>
2018-01-31 21:47:09 +0100

    Make dates in result table nicer.

--- src/components/TableResult.js
+++ src/components/TableResult.js
@@ -6,7 +6,18 @@ export default function JsonResult({ value }) {
     return '';
   }
 
-  const { rows } = JSON.parse(value);
+  let { rows } = JSON.parse(value);
+
+  // sort by timestamp and convert timestamp to dates
+  if (rows[0] && rows[0].length > 1 && !isNaN(parseInt(rows[0][0], 10))) {
+    rows = rows
+      .sort((r1, r2) => r1[0] - r2[0])
+      .map(([timestamp, ...cols]) => {
+        const formattedDate = new Date(timestamp).toLocaleString();
+        return [formattedDate, ...cols];
+      })
+  }
+
   return (
     <Table striped bordered condensed>
       <tbody>
